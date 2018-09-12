af51bc20824056a888b7804bcae0ed834326bc31
Huijie Wei <huijiewei@outlook.com>
2018-09-12 11:30:35 +0800

    修复 locale

--- src/MomentAsset.php
+++ src/MomentAsset.php
@@ -15,6 +15,9 @@ class MomentAsset extends AssetBundle
 {
     public $sourcePath = '@npm/moment';
 
+    public $locale;
+    public $setLocaleOnReady = true;
+
     public $js = [
         'min/moment.min.js',
     ];
@@ -26,43 +29,58 @@ class MomentAsset extends AssetBundle
         ]
     ];
 
-    public function registerAssetFiles($view)
+    public function init()
     {
-        $locale = strtolower(\Yii::$app->language);
+        parent::init();
 
-        if ($locale != 'en') {
-            $sourcePath = \Yii::getAlias($this->sourcePath);
-
-            $desiredFile = $this->getDesiredFile($sourcePath, $locale);
-
-            if (!is_file($desiredFile)) {
-                $fallbackLocale = substr($locale, 0, 2);
-
-                if ($fallbackLocale != 'en') {
-                    $desiredFile = $this->getDesiredFile($sourcePath, $fallbackLocale);
-
-                    if (file_exists($desiredFile)) {
-                        $locale = $fallbackLocale;
-                    }
-                }
-            }
-
-            if ($locale != 'en') {
-                $this->js[] = 'locale/' . $locale . '.js';
-            }
-        }
-
-        parent::registerAssetFiles($view);
-
-        if ($locale != 'en') {
-            $js = "moment.locale('{$locale}')";
-
-            $view->registerJs($js, View::POS_READY, 'moment-locale-' . $locale);
+        if ($this->locale === null) {
+            $this->locale = \Yii::$app->language;
         }
     }
 
-    private function getDesiredFile($sourcePath, $locale)
+    public function registerLocaleInternal($view)
     {
-        return $sourcePath . DIRECTORY_SEPARATOR . 'locale' . DIRECTORY_SEPARATOR . "{$locale}.js";
+        $localeFilePath = $this->tryFindLocale();
+
+        if (YII_DEBUG && !$localeFilePath) {
+            throw new InvalidConfigException('Locale file "' . \Yii::$app->language . '" not exists!');
+        }
+
+        $manager = $view->getAssetManager();
+
+        $view->registerJsFile(
+            $manager->getAssetUrl($this, $this->locale),
+            $this->jsOptions,
+            'moment-locale-' . $this->locale
+        );
+
+        if ($this->setLocaleOnReady) {
+            $js = "moment().locale('" . $this->locale . "');";
+            $view->registerJs($js, View::POS_READY, 'moment-set-default-locale');
+        }
+    }
+
+    private function tryFindLocale()
+    {
+        $localeFile = substr(strtolower($this->locale), 0, 2) . '.js';
+        $localeFilePath = "{$this->sourcePath}/$localeFile";
+
+        if (!file_exists($localeFilePath)) {
+            $localeFile = substr(strtolower($this->locale), 0, 5) . '.js';
+            $localeFilePath = "{$this->sourcePath}/$localeFile";
+
+            if (!file_exists($localeFilePath)) {
+                $localeFile = substr(strtolower($this->locale), 3, 5) . '.js';
+                $localeFilePath = "{$this->sourcePath}/$localeFile";
+
+                if (!file_exists($localeFilePath)) {
+                    return false;
+                }
+            }
+        }
+
+        $this->locale = $localeFile;
+
+        return $localeFilePath;
     }
 }
